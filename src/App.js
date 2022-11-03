729b651ddaa5a122c1ef5b477b289a0104c3a96d
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-03 20:02:30 +0900

    feat: 사용자에게 숫자를 입력받는다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const printToConsole = (message) => {
+  MissionUtils.Console.print(message);
+};
+
+const readLine = (message, callback) => {
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', callback);
+};
 class App {
-  play() {}
+  play() {
+    printToConsole('숫자 야구 게임을 시작합니다.');
+    readLine('숫자를 입력해주세요 : ', (answer) => {
+      console.log(answer);
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
