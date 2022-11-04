d26b1fa0d41e39317bc0d1db5fc5e9c5567f6349
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-04 14:55:35 +0900

    test: 중복되는 숫자가 있는지 체크하는 함수

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,5 +1,12 @@
-const { isWrongLength } = require('../src/utils/validate.js');
+const { isWrongLength, hasDuplicatedNumbers } = require('../src/utils/validate.js');
 
-test('올바른 길이의 숫자를 입력하였는가?', () => {
-  expect(() => isWrongLength('1234')).toThrow(Error);
+describe('예외처리 테스트', () => {
+  test('올바른 길이의 숫자를 입력하였는가?', () => {
+    expect(() => isWrongLength('1234')).toThrow(Error);
+  });
+
+  test('중복없이 숫자를 입력하였는가?', () => {
+    expect(() => hasDuplicatedNumbers('133')).toThrow(Error);
+    expect(() => hasDuplicatedNumbers('333')).toThrow(Error);
+  });
 });
