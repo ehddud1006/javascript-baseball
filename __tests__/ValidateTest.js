d5874fd0ab9da19b240f65e4da66706d6adacc3d
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-04 14:45:48 +0900

    test: 입력 숫자가 3자리수 인지 체크하는 함수

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,7 +1,5 @@
-const isRightLength = (num) => {
-  if (num.length !== 3) throw new Error('잘못된 데이터 형식입니다.');
-};
+const { isWrongLength } = require('../src/utils/validate.js');
 
 test('올바른 길이의 숫자를 입력하였는가?', () => {
-  expect(() => isRightLength('1234')).toThrow(Error);
+  expect(() => isWrongLength('1234')).toThrow(Error);
 });
