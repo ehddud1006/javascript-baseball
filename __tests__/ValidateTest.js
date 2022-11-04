5505183f7397d012701960fa97989e9cfcc515f6
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-04 15:00:46 +0900

    test: 0이 포함되어있는지 체크하는 함수

--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,4 +1,4 @@
-const { isWrongLength, hasDuplicatedNumbers } = require('../src/utils/validate.js');
+const { isWrongLength, hasDuplicatedNumbers, hasZero } = require('../src/utils/validate.js');
 
 describe('예외처리 테스트', () => {
   test('올바른 길이의 숫자를 입력하였는가?', () => {
@@ -8,5 +8,9 @@ describe('예외처리 테스트', () => {
   test('중복없이 숫자를 입력하였는가?', () => {
     expect(() => hasDuplicatedNumbers('133')).toThrow(Error);
     expect(() => hasDuplicatedNumbers('333')).toThrow(Error);
+  });
+
+  test('0이 포함되어 있는가?', () => {
+    expect(() => hasZero('102')).toThrow(Error);
   });
 });
