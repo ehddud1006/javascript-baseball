df0aa6c1b5cf669c9605a219b26077628c7ef347
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-04 14:45:08 +0900

    feat: 입력 숫자가 3자리수 인지 체크하는 함수

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ const readLine = (message, callback) => {
   MissionUtils.Console.readLine('숫자를 입력해주세요 : ', callback);
 };
 
-const isRightLength = (num) => {
-  return num.length === 3;
-};
-
 class App {
   play() {
     printToConsole('숫자 야구 게임을 시작합니다.');
---
+++ src/utils/validate.js
@@ -0,0 +1,5 @@
+const isWrongLength = (num) => {
+  if (num.length !== 3) throw new Error('3자리의 숫자를 입력해주세요');
+};
+
+module.exports = { isWrongLength };
