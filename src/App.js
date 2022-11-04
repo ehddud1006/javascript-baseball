f6f014d278f02683286857bd01f05c16c97b6fcb
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-04 17:02:07 +0900

    feat: 서로 다른 3개의 숫자 생성 함수

--- src/App.js
+++ src/App.js
@@ -9,13 +9,27 @@ const readLine = (message, callback) => {
   MissionUtils.Console.readLine('숫자를 입력해주세요 : ', callback);
 };
 
+const generateRandomNumberArray = () => {
+  const randomNumberArray = [];
+  while (randomNumberArray.length < 3) {
+    const digit = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!randomNumberArray.includes(digit)) {
+      randomNumberArray.push(digit);
+    }
+  }
+  return randomNumberArray;
+};
+
 class App {
   play() {
     printToConsole('숫자 야구 게임을 시작합니다.');
+    const randomNumberArray = generateRandomNumberArray();
     readLine('숫자를 입력해주세요 : ', (answer) => {
       console.log(answer);
       try {
-        if (isValidNumbers(answer)) console.log('hello');
+        if (isValidNumbers(answer)) {
+          const userInputNumber = answer.split('').map((v) => Number(v));
+        }
       } catch (error) {
         console.log(error);
       }
